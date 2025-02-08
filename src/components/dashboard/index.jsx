9a1d15a61feb9d6b1eea3831aa57be2ac44edbe0
BrokenTime9 <cerberuscraniax@gmail.com>
2025-02-08 08:42:58 +0530

    scroll fix 3

--- src/components/dashboard/index.jsx
+++ src/components/dashboard/index.jsx
@@ -11,132 +11,132 @@ import UrlContext from "../context/urlContext";
 import MobileContext from "./context/mobileWidth";
 
 const Dashboard = () => {
-  const [isFormVisible, setIsFormVisible] = useState(false);
-  const [chatId, setChatId] = useState([]);
-  const [user, setUser] = useState("");
-  const [isChatVisible, setIsChatVisible] = useState(true);
-  const [show, setShow] = useState(true);
-  const [ws, setWs] = useState("");
-  const { url } = useContext(UrlContext);
-  const { isMobile } = useContext(MobileContext);
+	const [isFormVisible, setIsFormVisible] = useState(false);
+	const [chatId, setChatId] = useState([]);
+	const [user, setUser] = useState("");
+	const [isChatVisible, setIsChatVisible] = useState(true);
+	const [show, setShow] = useState(true);
+	const [ws, setWs] = useState("");
+	const { url } = useContext(UrlContext);
+	const { isMobile } = useContext(MobileContext);
 
-  useEffect(() => {
-    const checkLogin = async () => {
-      const resp = await axios.post(
-        `${url}/api/auth/checkLogin`,
-        {},
-        { withCredentials: true },
-      );
-      if (resp?.data?.redirectTo) {
-        window.location.href = "/";
-      }
-    };
+	useEffect(() => {
+		const checkLogin = async () => {
+			const resp = await axios.post(
+				`${url}/api/auth/checkLogin`,
+				{},
+				{ withCredentials: true },
+			);
+			if (resp?.data?.redirectTo) {
+				window.location.href = "/";
+			}
+		};
 
-    checkLogin();
-  }, []);
-  useEffect(() => {
-    axios
-      .post(`${url}/api/user`, {}, { withCredentials: true })
-      .then((data) => {
-        setUser(data.data.username);
-      })
-      .catch((e) => {
-        console.error(e);
-      });
-  }, []);
+		checkLogin();
+	}, []);
+	useEffect(() => {
+		axios
+			.post(`${url}/api/user`, {}, { withCredentials: true })
+			.then((data) => {
+				setUser(data.data.username);
+			})
+			.catch((e) => {
+				console.error(e);
+			});
+	}, []);
 
-  const handleLogOut = async () => {
-    const res = await axios.post(
-      `${url}/api/auth/logout`,
-      {},
-      { withCredentials: true },
-    );
+	const handleLogOut = async () => {
+		const res = await axios.post(
+			`${url}/api/auth/logout`,
+			{},
+			{ withCredentials: true },
+		);
 
-    if (res?.data?.redirectTo) {
-      window.location.href = "/";
-    }
-  };
-  const handleAdd = () => {
-    setIsFormVisible((prevState) => !prevState);
-  };
-  const toggleChatVisibility = () => {
-    setIsChatVisible((prevState) => !prevState);
-  };
-  return (
-    <UserContext.Provider value={{ user, setUser }}>
-      <IdContext.Provider value={{ chatId, setChatId }}>
-        <WsContext.Provider value={{ ws, setWs }}>
-          {show ? (
-            <div className="flex min-h-[100dvh] w-screen">
-              {isChatVisible ? (
-                <div
-                  className="bg-gradient-to-r from-gray-700 to-gray-800 text-white p-6 flex-col justify-between shadow-lg"
-                  style={{
-                    width: isMobile
-                      ? !chatId.length > 0
-                        ? "100vw"
-                        : "0"
-                      : "24rem",
-                    display: isMobile
-                      ? !chatId.length > 0
-                        ? "flex"
-                        : "none"
-                      : "flex",
-                  }}
-                >
-                  <div className="flex flex-row text-xl font-semibold mb-5 justify-between">
-                    <h1>Chats</h1>
-                    <h1>⫶</h1>
-                  </div>
-                  <ShowChats addF={isFormVisible} />
-                  <div
-                    onClick={handleAdd}
-                    className="p-3 mt-3 mb-1 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600 cursor-pointer text-center text-xl font-semibold"
-                  >
-                    Add friend
-                  </div>
+		if (res?.data?.redirectTo) {
+			window.location.href = "/";
+		}
+	};
+	const handleAdd = () => {
+		setIsFormVisible((prevState) => !prevState);
+	};
+	const toggleChatVisibility = () => {
+		setIsChatVisible((prevState) => !prevState);
+	};
+	return (
+		<UserContext.Provider value={{ user, setUser }}>
+			<IdContext.Provider value={{ chatId, setChatId }}>
+				<WsContext.Provider value={{ ws, setWs }}>
+					{show ? (
+						<div className="flex max-h-[100dvh] w-screen">
+							{isChatVisible ? (
+								<div
+									className="bg-gradient-to-r from-gray-700 to-gray-800 text-white p-6 flex-col justify-between shadow-lg"
+									style={{
+										width: isMobile
+											? !chatId.length > 0
+												? "100vw"
+												: "0"
+											: "24rem",
+										display: isMobile
+											? !chatId.length > 0
+												? "flex"
+												: "none"
+											: "flex",
+									}}
+								>
+									<div className="flex flex-row text-xl font-semibold mb-5 justify-between">
+										<h1>Chats</h1>
+										<h1>⫶</h1>
+									</div>
+									<ShowChats addF={isFormVisible} />
+									<div
+										onClick={handleAdd}
+										className="p-3 mt-3 mb-1 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600 cursor-pointer text-center text-xl font-semibold"
+									>
+										Add friend
+									</div>
 
-                  <div
-                    onClick={handleLogOut}
-                    className="p-3 mt-3 mb-1 bg-red-500 text-white rounded-lg shadow-md hover:bg-red-600 cursor-pointer text-center text-xl font-semibold"
-                  >
-                    Logout
-                  </div>
-                </div>
-              ) : (
-                ""
-              )}
-              <div className="flex flex-col  flex-grow bg-gray-100 rounded-lg shadow-lg">
-                {chatId.length > 1 ? (
-                  <>
-                    <ChatHeader
-                      mobile={isMobile}
-                      chat={isChatVisible}
-                      chatVisibility={
-                        isMobile ? toggleChatVisibility : undefined
-                      }
-                    />
-                  </>
-                ) : (
-                  ""
-                )}
-                <ShowMessages mob={isMobile} />
-                {chatId.length > 1 ? (
-                  <>
-                    <SendMessage />
-                  </>
-                ) : (
-                  ""
-                )}
-              </div>
-            </div>
-          ) : (
-            ""
-          )}
-        </WsContext.Provider>
-      </IdContext.Provider>
-    </UserContext.Provider>
-  );
+									<div
+										onClick={handleLogOut}
+										className="p-3 mt-3 mb-1 bg-red-500 text-white rounded-lg shadow-md hover:bg-red-600 cursor-pointer text-center text-xl font-semibold"
+									>
+										Logout
+									</div>
+								</div>
+							) : (
+								""
+							)}
+							<div className="flex flex-col  flex-grow bg-gray-100 rounded-lg shadow-lg">
+								{chatId.length > 1 ? (
+									<>
+										<ChatHeader
+											mobile={isMobile}
+											chat={isChatVisible}
+											chatVisibility={
+												isMobile ? toggleChatVisibility : undefined
+											}
+										/>
+									</>
+								) : (
+									""
+								)}
+								<ShowMessages mob={isMobile} />
+								{chatId.length > 1 ? (
+									<>
+										<SendMessage />
+									</>
+								) : (
+									""
+								)}
+							</div>
+						</div>
+					) : (
+						""
+					)}
+				</WsContext.Provider>
+			</IdContext.Provider>
+		</UserContext.Provider>
+	);
 };
 
 export default Dashboard;
